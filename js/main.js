4b217b4423e2274942961ac424e00a062545aa51
mark-starikov <supermark2005@mail.ru>
2023-07-13 20:09:38 +0700

    fix: icons svg sprite and navbar menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo-svg use");
 
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
         navbar.classList.add("navbar-light");
+        logo.href.baseVal = "img/svg/sprites.svg#logo"
     } else {
         navbar.classList.remove("navbar-light");
+        logo.href.baseVal = "img/svg/sprites.svg#logo-light"
     }
 });
