843f1efb946c28f941239cbcc29ca0d241b10d9e
mark-starikov <supermark2005@mail.ru>
2023-07-15 15:28:13 +0700

    add: button menu-toggle

--- js/main.js
+++ js/main.js
@@ -3,17 +3,31 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".navbar-menu-toggle");
 const menu = document.querySelector(".navbar-mobile");
 
+const openMenu = (event) => { // Стрелочная функция открывания меню
+    menu.classList.add("is-open"); // Добавляем класс к меню
+    mMenuToggle.classList.add("close-menu"); // Добавляем класс к кнопке меню
+    document.body.style.overflow = "hidden"; // Когда меню открыто, страница не двигается
+    navbarLight(); // Вызываем функцию светлой шапки
+};
+const closeMenu = (event) => { // Функция закрытия меню
+    menu.classList.remove("is-open"); // Удаляем класс с меню
+    mMenuToggle.classList.remove("close-menu"); // Удаляем класс с кнопки меню
+    document.body.style.overflow = ""; // Страница двигается
+    navbarDark(); // Функция темной шапки
+};
+const navbarLight = (event) => {
+    navbar.classList.add("navbar-light");
+    logo.href.baseVal = "img/svg/sprites.svg#logo";
+};
+const navbarDark = (event) => {
+    navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/svg/sprites.svg#logo-light";
+}
 
-window.addEventListener('scroll', () => {
-    if (this.scrollY > 1) {
-        navbar.classList.add("navbar-light");
-        logo.href.baseVal = "img/svg/sprites.svg#logo"
-    } else {
-        navbar.classList.remove("navbar-light");
-        logo.href.baseVal = "img/svg/sprites.svg#logo-light"
-    }
+window.addEventListener('scroll', () => { // Добавляем к окну слушателя, при скроле страницы выполняем фукцию
+    this.scrollY > 1 ? navbarLight() : navbarDark(); // Если страница покручивается на 1 пиксель, включается светлая шапка, в противном случае темная шапка 
 });
-mMenuToggle.addEventListener("click", (event) => {
-    event.preventDefault();
-    menu.classList.toggle("is-open");
+mMenuToggle.addEventListener("click", (event) => { // Функция при клике на кнопку меню
+    event.preventDefault(); // Отключаем переход по ссылке для кнопки меню
+    menu.classList.contains("is-open") ? closeMenu() : openMenu(); // если меню содержит класс is-open, выполняется функция закрытия меню, в противном случае меню открывается.
 });
