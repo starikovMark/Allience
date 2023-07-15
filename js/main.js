65caf9277397e559cf4e306387a2d3aeb8d6bec1
mark-starikov <supermark2005@mail.ru>
2023-07-15 14:11:14 +0700

    add: mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".navbar-menu-toggle");
+const menu = document.querySelector(".navbar-mobile");
+
 
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
@@ -9,4 +12,8 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove("navbar-light");
         logo.href.baseVal = "img/svg/sprites.svg#logo-light"
     }
+});
+mMenuToggle.addEventListener("click", (event) => {
+    event.preventDefault();
+    menu.classList.toggle("is-open");
 });
