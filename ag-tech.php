6010e08e124f0a85058be246ff133d624ff7dbb3
mark-starikov <supermark2005@mail.ru>
2023-08-18 14:44:07 +0700

    add: ag-tech text block

---
+++ ag-tech.php
@@ -0,0 +1,13 @@
+<?php
+$page_title = "Автохимия AG-TECH";
+$header_image = "named-image-visible";
+include_once("./templates/header-page-block.php") ?>
+<?php
+$section_title = "Собственное производство автохимия AG-Tech";
+$section_image = "ag-tech-page-image";
+include_once("./templates/text-page-block.php") ?>
+<div class="container">
+    <?php
+    $button_color = "slider-button-dark";
+    include_once("./templates/swiper-block.php") ?>
+</div>
--- auto-him.php
+++ auto-him.php
@@ -2,48 +2,15 @@
 $page_title = "Автомобильная химия";
 $header_image = "named-image-visible";
 include_once("./templates/header-page-block.php") ?>
-<section class="section auto section-auto">
-    <div class="container">
-        <div class="auto-wrapper">
-            <picture class="auto-image">
-                <source type="image/webp" srcset="img/png/blog-image-1.webp">
-                <source type="image/jpeg" srcset="img/png/blog-image-1.png">
-                <img src="img/png/blog-image-1.png" alt="png: blog-image-1" class="auto-image">
-            </picture>
-            <div class="auto-content">
-                <div class="seporator"></div>
-                <h2 class="section-title auto-content-title">Контрактное производство автомобильной химии</h2>
-                <p class="section-text auto-content-text">Прежде всего, начало повседневной работы по формированию
-                    позиции
-                    однозначно фиксирует необходимость приоритизации разума над эмоциями. Кстати, представители
-                    современных
-                    социальных резервов неоднозначны и будут описаны максимально подробно.
-                </p>
-                <p class="section-text auto-content-text"> Но социально-экономическое развитие напрямую зависит от
-                    поставленных обществом задач! Безусловно,
-                    постоянный количественный рост и сфера нашей активности предоставляет широкие возможности для
-                    системы
-                    обучения кадров, соответствующей насущным потребностям. В целом, конечно, выбранный нами
-                    инновационный
-                    путь обеспечивает широкому кругу (специалистов) участие в формировании своевременного выполнения
-                    сверхзадачи. А ещё элементы политического процесса представлены в исключительно положительном свете.
-                    Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого
-                    факта: высокотехнологичная концепция общественного уклада создаёт предпосылки для глубокомысленных
-                    рассуждений. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие
-                    различных
-                    форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты
-                    для
-                    поставленных обществом задач.</p>
-            </div>
-            <!-- /.auto-content -->
-        </div>
-        <?php
-        $button_color = "slider-button-dark";
-        include_once("./templates/swiper-block.php") ?>
-    </div>
-    <!-- /.container -->
-</section>
-<!-- /.section auto section-auto -->
+<?php
+$section_title = "Контрактное производство автомобильной химии";
+$section_image = "blog-image-1";
+include_once("./templates/text-page-block.php") ?>
+<div class="container">
+    <?php
+    $button_color = "slider-button-dark";
+    include_once("./templates/swiper-block.php") ?>
+</div>
 <section class="section manufacture section-manufacture">
     <div class="container">
         <div class="manufacture-wrapper">
